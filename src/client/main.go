c893f02cd42441737d7c359456be4309ef3e86f9
netwave <3danimanimal@gmail.com>
2018-08-22 20:22:21 +0200

    gorutines working on Sleep example

--- src/client/main.go
+++ src/client/main.go
@@ -5,20 +5,37 @@ import (
 	"fmt"
 	"log"
 	"net/rpc"
+	"sync"
 )
 
 type ConnClient struct {
-	client *rpc.Client
+	Client         *rpc.Client
+	RecieveChannel chan common.CommReply
+	WaitingGroup   sync.WaitGroup
 }
 
-func (cc *ConnClient) callSleep(seconds uint) common.CommReply {
-	args := &common.CommSleepArg{Seconds: seconds}
-	var reply common.CommReply
-	err := cc.client.Call("SimpleOperation.Sleep", args, &reply)
-	if err != nil {
-		log.Fatal("arith error:", err)
-	}
-	return reply
+var RecieveChannel = make(chan common.CommReply)
+
+func (cc *ConnClient) callSleep(seconds uint) {
+	cc.WaitingGroup.Add(1)
+	go func() {
+		defer cc.WaitingGroup.Done()
+		args := &common.CommSleepArg{Seconds: seconds}
+		var reply common.CommReply
+		err := cc.Client.Call("SimpleOperation.Sleep", args, &reply)
+		if err != nil {
+			log.Fatal("arith error:", err)
+		}
+		cc.RecieveChannel <- reply
+	}()
+}
+
+func (cc *ConnClient) asArrive() {
+	go func() {
+		for reply := range cc.RecieveChannel {
+			fmt.Println(reply.Message)
+		}
+	}()
 }
 
 func main() {
@@ -26,7 +43,11 @@ func main() {
 	if err != nil {
 		log.Fatal("dialing:", err)
 	}
-	connclient := &ConnClient{client: client}
+	connclient := &ConnClient{Client: client, RecieveChannel: RecieveChannel, WaitingGroup: sync.WaitGroup{}}
 
-	fmt.Println(connclient.callSleep(10))
+	connclient.asArrive()
+	for i := 10; i >= 0; i-- {
+		connclient.callSleep(uint(i))
+	}
+	connclient.WaitingGroup.Wait()
 }
--- src/common/comm_implementation.go
+++ src/common/comm_implementation.go
@@ -12,7 +12,7 @@ type SipleOperation struct {
 
 func (so *SipleOperation) Sleep(arg *CommSleepArg, reply *CommReply) error {
 	time.Sleep(time.Duration(arg.Seconds) * time.Second)
-	fmt.Fprintf(os.Stdout, "Finished waiting %d", arg.Seconds)
+	fmt.Fprintf(os.Stdout, "Finished waiting %d\n", arg.Seconds)
 	reply.Message = fmt.Sprintf("Finished waiting %d", arg.Seconds)
 	return nil
 }
@@ -21,10 +21,10 @@ func (so *SipleOperation) SimpleOperation(arg *CommSimpleOperationArg, reply *Co
 	f, ok := so.available_operations[arg.Operator]
 	if ok {
 		res := f(arg.A, arg.B)
-		fmt.Fprintf(os.Stdout, "Computed result (%d %s %d = %d)", arg.A, arg.Operator, arg.B, res)
+		fmt.Fprintf(os.Stdout, "Computed result (%d %s %d = %d)\n", arg.A, arg.Operator, arg.B, res)
 		reply.Message = fmt.Sprintf("Computed result (%d %s %d = %d)", arg.A, arg.Operator, arg.B, res)
 	} else {
-		fmt.Fprintf(os.Stdout, "Could not match any operator %s", arg.Operator)
+		fmt.Fprintf(os.Stdout, "Could not match any operator %s\n", arg.Operator)
 		reply.Message = fmt.Sprintf("Could not match any operator %s", arg.Operator)
 	}
 	return nil
