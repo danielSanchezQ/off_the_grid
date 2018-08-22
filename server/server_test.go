043812cc3a366ba6ceac5a75eefda72b2299cae8
netwave <3danimanimal@gmail.com>
2018-08-22 13:40:13 +0200

    added base server dispatcher

--- server/server_test.go
+++ server/server_test.go
@@ -1,1 +1,26 @@
 package server
+
+import (
+	"fmt"
+	"net/http"
+)
+
+var PORT = "8080"
+
+type ChoseRequestMux struct {
+	requestType string
+}
+
+func (mux *ChoseRequestMux) Dispatch(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello world")
+}
+
+func (mux *ChoseRequestMux) ServeHttp(w http.ResponseWriter, r *http.Request) {
+	switch method := r.Method; method {
+	case mux.requestType:
+		mux.Dispatch(w, r)
+	default:
+		http.Redirect(w, r, "/", http.StatusFound)
+	}
+
+}
