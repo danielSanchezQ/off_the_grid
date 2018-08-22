358b52985042ce1058eb19b48e2bbb7c920e256d
netwave <3danimanimal@gmail.com>
2018-08-22 14:33:56 +0200

    added base routing actions example working on regex redirection

--- server/main.go
+++ server/main.go
@@ -3,16 +3,30 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"regexp"
 )
 
 var PORT = "8080"
 
 type ChoseRequestMux struct {
 	requestType string
+	routes      map[string]func(http.ResponseWriter, *http.Request)
 }
 
 func (mux *ChoseRequestMux) Dispatch(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello world")
+	match := false
+	for pattern, f := range mux.routes {
+		innerMatch, _ := regexp.MatchString(pattern, r.URL.String())
+		if innerMatch {
+			f(w, r)
+			break
+		} else {
+			match = innerMatch
+		}
+	}
+	if !match {
+		http.Redirect(w, r, "", http.StatusBadRequest)
+	}
 }
 
 func (mux *ChoseRequestMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -20,11 +34,16 @@ func (mux *ChoseRequestMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	case mux.requestType:
 		mux.Dispatch(w, r)
 	default:
-		http.Redirect(w, r, "/", http.StatusFound)
+		fmt.Fprint(w, r.URL.String())
+		//http.Redirect(w, r, "", http.StatusNotFound)
 	}
 }
 
 func main() {
 	fmt.Print("Launching server")
-	http.ListenAndServe(":"+PORT, &ChoseRequestMux{requestType: http.MethodPost})
+	var AppRoutes = make(map[string]func(http.ResponseWriter, *http.Request))
+	AppRoutes["/"] = func(w http.ResponseWriter, r *http.Request) { fmt.Fprint(w, "Hello world") }
+
+	mux := &ChoseRequestMux{requestType: http.MethodPost, routes: AppRoutes}
+	http.ListenAndServe(":"+PORT, mux)
 }
